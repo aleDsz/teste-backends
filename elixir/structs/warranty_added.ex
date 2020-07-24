ec3b1cc72122d17ba82eef13103c246415f6fe0c
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-24 02:15:59 -0300

    Add struct for warranty.added

---
+++ elixir/structs/warranty_added.ex
@@ -0,0 +1,16 @@
+defmodule WarrantyAdded do
+  @fields ~w(
+    event_id
+    event_schema
+    event_action
+    event_timestamp
+    proposal_id
+    warranty_id
+    warranty_value
+    warranty_province
+  )a
+
+  defstruct @fields
+
+  def get_header(), do: @fields
+end
