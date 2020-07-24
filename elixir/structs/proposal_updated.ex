eb7ea9f604449281ac941386ca9d0f9def502022
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-24 02:15:37 -0300

    Add struct for proposal.updated

---
+++ elixir/structs/proposal_updated.ex
@@ -0,0 +1,15 @@
+defmodule ProposalUpdated do
+  @fields ~w(
+    event_id
+    event_schema
+    event_action
+    event_timestamp
+    proposal_id
+    proposal_loan_value
+    proposal_number_of_monthly_installments
+  )a
+
+  defstruct @fields
+
+  def get_header(), do: @fields
+end
